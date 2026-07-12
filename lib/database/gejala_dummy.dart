108b6b103097afe74e6a6063087a10d9b055a6eb
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2026-07-13 04:36:59 +0800

    Data Gejala sementara

---
+++ lib/database/gejala_dummy.dart
@@ -0,0 +1,39 @@
+import '../../models/gejala.dart';
+
+class GejalaDummy {
+
+  static List<Gejala> data = [
+
+    Gejala(
+      id: 1,
+      code: "G01",
+      question: "Apakah kulit terasa berminyak setelah mencuci wajah?",
+    ),
+
+    Gejala(
+      id: 2,
+      code: "G02",
+      question: "Apakah pori-pori terlihat besar?",
+    ),
+
+    Gejala(
+      id: 3,
+      code: "G03",
+      question: "Apakah wajah tampak mengkilap sepanjang hari?",
+    ),
+
+    Gejala(
+      id: 4,
+      code: "G04",
+      question: "Apakah kulit terasa kering setelah mencuci wajah?",
+    ),
+
+    Gejala(
+      id: 5,
+      code: "G05",
+      question: "Apakah kulit sering mengelupas?",
+    ),
+
+  ];
+
+}
