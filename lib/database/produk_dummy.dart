52e7fd33fd5ee55f1c27b64a957d22804c844e30
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2026-07-13 23:27:59 +0800

    Data produk sementara

---
+++ lib/database/produk_dummy.dart
@@ -0,0 +1,44 @@
+import '../../models/produkSkincare.dart';
+
+class SkincareProductDummy {
+
+  static List<SkincareProduct> data = [
+
+    SkincareProduct(
+      id: 1,
+      name: "Salicylic Acid Facial Wash",
+      category: "Facial Wash",
+      description: "Membersihkan minyak berlebih.",
+    ),
+
+    SkincareProduct(
+      id: 2,
+      name: "Tea Tree Toner",
+      category: "Toner",
+      description: "Menenangkan kulit berjerawat.",
+    ),
+
+    SkincareProduct(
+      id: 3,
+      name: "Niacinamide Serum",
+      category: "Serum",
+      description: "Mengurangi noda hitam.",
+    ),
+
+    SkincareProduct(
+      id: 4,
+      name: "Oil Free Moisturizer",
+      category: "Moisturizer",
+      description: "Menjaga kelembapan kulit.",
+    ),
+
+    SkincareProduct(
+      id: 5,
+      name: "Matte Sunscreen SPF50",
+      category: "Sunscreen",
+      description: "Melindungi kulit dari sinar UV.",
+    ),
+
+  ];
+
+}
