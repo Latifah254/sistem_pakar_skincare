a29357e9283a00e78cf79163dc9186d6d8ec6db5
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2026-07-13 23:21:16 +0800

    Data jenis kulit sementara

---
+++ lib/database/jenisKulit_dummy.dart
@@ -0,0 +1,39 @@
+import '../../models/jenisKulit.dart';
+
+class SkinTypeDummy {
+
+  static List<SkinType> data = [
+
+    SkinType(
+      id: 1,
+      code: "JK01",
+      name: "Kulit Berminyak",
+    ),
+
+    SkinType(
+      id: 2,
+      code: "JK02",
+      name: "Kulit Kering",
+    ),
+
+    SkinType(
+      id: 3,
+      code: "JK03",
+      name: "Kulit Kombinasi",
+    ),
+
+    SkinType(
+      id: 4,
+      code: "JK04",
+      name: "Kulit Sensitif",
+    ),
+
+    SkinType(
+      id: 5,
+      code: "JK05",
+      name: "Kulit Normal",
+    ),
+
+  ];
+
+}
