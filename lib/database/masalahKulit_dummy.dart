aaff8ae3ad8bbb83d926707cd436b057c632371f
Latifah254 <latifah.qalbi23@student.uisi.ac.id>
2026-07-13 23:23:31 +0800

    Data masalah kulit sementara

---
+++ lib/database/masalahKulit_dummy.dart
@@ -0,0 +1,39 @@
+import '../../models/masalahKulit.dart';
+
+class SkinProblemDummy {
+
+  static List<SkinProblem> data = [
+
+    SkinProblem(
+      id: 1,
+      code: "MK01",
+      name: "Jerawat",
+    ),
+
+    SkinProblem(
+      id: 2,
+      code: "MK02",
+      name: "Kulit Kusam",
+    ),
+
+    SkinProblem(
+      id: 3,
+      code: "MK03",
+      name: "Flek Hitam",
+    ),
+
+    SkinProblem(
+      id: 4,
+      code: "MK04",
+      name: "Iritasi",
+    ),
+
+    SkinProblem(
+      id: 5,
+      code: "MK05",
+      name: "Penuaan Dini",
+    ),
+
+  ];
+
+}
